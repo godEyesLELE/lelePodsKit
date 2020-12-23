c0519410558f738d4229a6e6f541656c49423b34
yc_htl <huangtianle@mbcloud.com>
2020-12-23 17:32:17 +0800

    开放open

--- lelePodsKit/Classes/TestView.swift
+++ lelePodsKit/Classes/TestView.swift
@@ -8,11 +8,17 @@
 import UIKit
 import SnapKit
 
-class TestView: UIView {
+open class TestView: UIView {
 
-    func testDemo(){
-        self.snp.makeConstraints { (it) in
-            
+    public func testDemo(){
+        self.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.width.lessThanOrEqualTo(300)
+            $0.top.equalToSuperview().offset(12)
+            $0.bottom.equalToSuperview().offset(-12)
+            $0.right.equalToSuperview().offset(-15)
+            $0.left.equalToSuperview().offset(8)
         }
+        self.backgroundColor = UIColor.red
     }
 }
